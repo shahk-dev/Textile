bf8fc823da2224c35f1aae3bdaf430bb1d2803b5
Yusuf <abrorov.muxriddin@gmail.com>
2025-07-09 17:32:05 +0500

    yusuf commit qildi

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,64 @@
-import { useTranslations } from 'next-intl';
-
+import Link from "next/link";
+import { navItems } from "@/utils/data";
+import { FaTelegramPlane } from "react-icons/fa";
+import { FaFacebookF } from "react-icons/fa";
+import { RiInstagramFill } from "react-icons/ri";
+import Logo from "./Logo";
 export default function Footer() {
-  const t = useTranslations('Footer');
-
   return (
-    <footer className="bg-gray-800 text-white p-4 mt-8">
-      <div className="container mx-auto text-center">
-        <p>© 2024 {t('company')}. {t('rights')}</p>
+    <footer className="bg-blue-900 mt-12 sm:mt-16">
+      <div className="container">
+        <div className="flex flex-col gap-y-12 py-12 sm:py-16 px-5">
+          <div className="flex flex-wrap gap-y-5 justify-between items-start">
+            <div className="flex flex-col gap-y-5 sm:gap-y-8">
+              <Logo />
+              <p className="text-white text-lg">
+                Leading the Way in Medical Execellence, Trusted Care.
+              </p>
+            </div>
+            <div className="flex flex-wrap gap-y-8 gap-x-15 justify-start lg:justify-center items-start">
+              <div className="flex flex-col gap-y-5 sm:gap-y-8">
+                <h2 className="text-lg font-semibold text-white">
+                  Important Links
+                </h2>
+                <div className="flex flex-col gap-y-2">
+                  {navItems.map((item, index) => {
+                    return (
+                      <p key={index} className="">
+                        <Link href={item.url} className="text-white text-lg">
+                          {item.name}
+                        </Link>
+                      </p>
+                    );
+                  })}
+                </div>
+              </div>
+              <div className="flex flex-col gap-y-5 sm:gap-y-8">
+                <h2 className="text-lg font-semibold text-white">Contact</h2>
+                <div className="flex flex-col gap-y-2">
+                  <Link href={"tel:+998901000001"} className="text-white">
+                    Phone: +998901000001
+                  </Link>
+                  <p className="text-white">Email: fildineesoe@gmail.com</p>
+                  <p className="text-white">Address: 0123 Some place</p>
+                  <p className="text-white">Some country</p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="w-full h-[1px] bg-blue-400"></div>
+          <div className="flex flex-col sm:flex-row justify-between gap-3">
+            <p className="text-[16px] text-white">
+              © 2025 Hospital’s name All Rights Reserved by Zumar Soft & Zumar Education
+            </p>
+            <div className="flex gap-x-3 items-center">
+              <FaTelegramPlane className="text-2xl text-blue-400" />
+              <FaFacebookF className="text-2xl text-blue-400" />
+              <RiInstagramFill className="text-2xl text-blue-400" />
+            </div>
+          </div>
+        </div>
       </div>
     </footer>
   );
---
+++ src/utils/data.ts
@@ -0,0 +1,96 @@
+export const contactData = [
+  {
+    id: 1,
+    image: "/images/card1.jpg",
+    name: "This Article’s Title goes Here, but not too long.",
+    date: "July 2, 2023",
+    views: "24.5K",
+  },
+  {
+    id: 2,
+    image: "/images/card2.jpg",
+    name: "This Article’s Title goes Here, but not too long.",
+    date: "July 2, 2023",
+    views: "24.5K",
+  },
+  {
+    id: 3,
+    image: "/images/card3.jpg",
+    name: "This Article’s Title goes Here, but not too long.",
+    date: "July 2, 2023",
+    views: "24.5K",
+  },
+];
+
+export const doctorsData = [
+  {
+    id: 1,
+    name: "Doctor’s Name",
+    image: "/images/card1.jpg",
+    job: "Neurology",
+  },
+  {
+    id: 2,
+    name: "Doctor’s Name",
+    image: "/images/card2.jpg",
+    job: "Neurology",
+  },
+  {
+    id: 3,
+    name: "Doctor’s Name",
+    image: "/images/card3.jpg",
+    job: "Neurology",
+  },
+  {
+    id: 4,
+    name: "Doctor’s Name",
+    image: "/images/card1.jpg",
+    job: "Neurology",
+  },
+];
+
+export const services = [
+  {
+    id: 1,
+    image: "/images/serviceImg.png",
+    name: "Free Checkup",
+    caption:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing Quisque placerat convallis felis vitae tortor augue. Velit nascetur massa in.",
+  },
+  {
+    id: 2,
+    image: "/images/serviceImg.png",
+    name: "Free Checkup",
+    caption:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing Quisque placerat convallis felis vitae tortor augue. Velit nascetur massa in.",
+  },
+  {
+    id: 3,
+    image: "/images/serviceImg.png",
+    name: "Free Checkup",
+    caption:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing Quisque placerat convallis felis vitae tortor augue. Velit nascetur massa in.",
+  },
+  {
+    id: 4,
+    image: "/images/serviceImg.png",
+    name: "Free Checkup",
+    caption:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing Quisque placerat convallis felis vitae tortor augue. Velit nascetur massa in.",
+  },
+  {
+    id: 5,
+    image: "/images/serviceImg.png",
+    name: "Free Checkup",
+    caption:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing Quisque placerat convallis felis vitae tortor augue. Velit nascetur massa in.",
+  },
+];
+
+export const navItems = [
+  { name: "Bosh Sahifa", url: "/", active: true },
+  { name: "Biz Haqimizda", url: "/about", active: false },
+  { name: "Xizmatlar", url: "/services", active: false },
+  { name: "Yangiliklar", url: "/news", active: false },
+  { name: "Aloqa", url: "/contact", active: false },
+];
