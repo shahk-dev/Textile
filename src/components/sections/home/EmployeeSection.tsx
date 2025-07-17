f14b7c58318703e0f34be9ccac2b431a0be91e76
Yusuf <abrorov.muxriddin@gmail.com>
2025-07-17 15:58:55 +0500

    employee section lang done

--- src/components/sections/home/EmployeeSection.tsx
+++ src/components/sections/home/EmployeeSection.tsx
@@ -1,18 +1,21 @@
 import Card from '@/components/shared/Card'
 import Slider from '@/components/shared/Slider'
 import { doctorsData } from '@/utils/data'
+import { useTranslations } from 'next-intl'
 
 export default function EmployeeSection() {
+    const t = useTranslations('HomePage');
+    // const [employee, setEmployee] = useState([]);
     return (
-        <section className="bg-white py-6">
+        <section id='employee'>
             <div className="container">
                 <div className="mt-12 sm:mt-20">
                     <div className="flex flex-col items-center gap-2">
-                        <h3 className="text-sm sm:text-lg uppercase tracking-[2.2px] text-blue-500 font-bold">
-                            Trusted Care
+                        <h3 className="text-sm sm:text-lg uppercase tracking-[2.2px] text-blue-500 font-semibold">
+                            {t('employee-desc')}
                         </h3>
-                        <h2 className="text-2xl sm:text-[32px] text-blue-900 capitalize">
-                            Our Doctors
+                        <h2 className="text-2xl sm:text-[32px] text-blue-900 uppercase font-bold">
+                            {t('employee-heading')}
                         </h2>
                     </div>
                     <div className="mt-12 sm:mt-16">
