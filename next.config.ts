50dce45a55aaaf94ffeee946ca14499a72a983b1
Yusuf <abrorov.muxriddin@gmail.com>
2025-07-17 22:34:36 +0500

    config file changed to /

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,7 @@ const nextConfig: NextConfig = {
             {
                 protocol: "https",
                 hostname: "perinatal.zumaredu.uz",
-                pathname: "**",
+                pathname: "/**",
             },
         ],
     },
